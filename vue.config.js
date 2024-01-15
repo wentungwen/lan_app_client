b5867dda18f60e23b0a423e1b9e095fa7d4cf36c
wentung <wentungwen@gmail.com>
2024-01-15 15:54:04 +0100

    fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   // publicPath: process.env.NODE_ENV === "production" ? "/lan-app-client-020ac5fe9106/" : "/",
   devServer: {
-    allowedHosts: "https://lan-app-client-020ac5fe9106.herokuapp.com/",
+    allowedHosts: "all",
   },
 });
